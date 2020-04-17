345b29b4e50de28bb1800f3a7574371f6f7f1e7c
anthony <anthony.gensane@gmail.com>
2020-04-17 09:33:59 +0200

    Creation composant CriteresContent

---
+++ src/components/CriteresContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FooterContent.css';
+
+function CriteresContent() {
+  return (
+    <div>
+      <div className="heigth" />
+    </div>
+  );
+}
+
+export default CriteresContent;
