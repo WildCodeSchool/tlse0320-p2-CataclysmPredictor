e81fc3b8e6322afcab823fd77e8559e3cbe32604
Mr-gateau <tagot.nicolas@gmail.com>
2020-04-22 10:51:35 +0200

    mise a jour du calend fonctionnel (Mauvais retour de date pour API)

--- src/calend.jsx
+++ src/calend.jsx
@@ -22,7 +22,6 @@ class Calend extends React.Component {
     this.state = {
       annee: 2020,
       mois: 0,
-      JourParMois: 30,
       showAppli: false,
       daySelect: 'Pas encore choisis',
       moiEnCour: [],
@@ -43,30 +42,36 @@ class Calend extends React.Component {
   }
 
   monthClick() {
-    const egal = this.state.mois + 1;
-
-    if (this.state.mois === 1) {
+    const mois = this.state.mois;
+    this.state.moiEnCour = [];
+    console.log(this.state.mois);
+    console.log(mois);
+    if (mois == 1) {
+      console.log('fevrier');
       if (
         this.state.annee % 4 === 0 &&
         (this.state.annee % 100 != 0 || this.state.annee % 400 === 0)
       ) {
         for (let i = 1; i <= 29; i++) {
-          this.setState({ moiEnCour: [this.state.moiEnCour.push(i)] });
+          this.state.moiEnCour.push(i);
         }
       } else {
         for (let i = 1; i <= 28; i++) {
-          this.setState({ moiEnCour: [this.state.moiEnCour.push(i)] });
+          this.state.moiEnCour.push(i);
         }
       }
-    } else if (egal === 1 || egal === 3 || egal === 5 || egal === 8 || egal === 10 || egal === 12) {
+    } else if (mois === 3 || mois === 5 || mois === 8 || mois === 10) {
+      console.log('30');
       for (let i = 1; i <= 30; i++) {
-        this.setState({ moiEnCour: [this.state.moiEnCour.push(i)] });
+        this.state.moiEnCour.push(i);
       }
     } else {
+      console.log('Else 31');
       for (let i = 1; i <= 31; i++) {
-        this.setState({ moiEnCour: [this.state.moiEnCour.push(i)] });
+        this.state.moiEnCour.push(i);
       }
     }
+    this.setState({ shMounth: false });
   }
 
   clickEnter() {
@@ -84,55 +89,53 @@ class Calend extends React.Component {
 
   reset() {
     const { annee } = this.state;
-    this.setState({ annee: 2020, mois: -1 });
+    this.setState({ annee: 2020 });
   }
+
+  returnFormat() {}
 
   render() {
     const { annee, mois, shMounth } = this.state;
-    {
-      this.monthClick();
-    }
     return (
       <div>
         // Gestion de l'année.
         <p className="text">{annee}</p>
         <button className="red" onClick={() => this.addYear(-10)}>
-          {' '}
-          --{' '}
+          --
         </button>
         <button className="red" onClick={() => this.addYear(-1)}>
-          {' '}
-          -{' '}
+          -
         </button>
         <button className="green" onClick={() => this.addYear(1)}>
-          {' '}
-          +{' '}
+          +
         </button>
         <button className="green" onClick={() => this.addYear(10)}>
-          {' '}
-          ++{' '}
+          ++
         </button>
         // Gestion des mois.
         <p className="text">{arrMois[this.state.mois]}</p>
-        <div className="red" onMouseEnter={this.showMonth} onMouseLeave={this.notShowMonth}>
+        <div className="red" onMouseEnter={this.showMonth}>
           <p className="text"> Select mois</p>
-
-          {this.state.shMounth ? (
-            <div>
-              {arrMois.map((moi, i) => (
-                <button className="green" onClick={() => this.setState({ mois: [i] })}>
-                  {arrMois[i]}
-                </button>
-              ))}
-            </div>
-          ) : null}
-          <p className="text">{mois} </p>
         </div>
+        {this.state.shMounth ? (
+          <div>
+            {arrMois.map((moi, i) => (
+              <button
+                className="green"
+                onMouseEnter={() => this.setState({ mois: i })}
+                onClick={this.monthClick}
+              >
+                {arrMois[i]}
+              </button>
+            ))}
+          </div>
+        ) : null}
+        <p className="text">{mois} </p>
         //le jour :
         {this.state.mois >= 0 ? (
           <div>
             <p className="text">
-              Quel jour ?{' '}
+              Quel jour ?
               {this.state.moiEnCour.map((jour, j) => (
                 <button className="green" onClick={() => this.setState({ daySelect: [j + 1] })}>
                   {jour}
@@ -143,7 +146,7 @@ class Calend extends React.Component {
           </div>
         ) : null}
         <p className="text">
-          Votre date choisi : {this.state.daySelect} {arrMois[this.state.mois]} {this.state.annee}
+          Votre date choisi : {this.state.annee}-{this.state.mois}-{this.state.daySelect}
         </p>
         <button onClick={this.reset}> Reset </button>
       </div>
