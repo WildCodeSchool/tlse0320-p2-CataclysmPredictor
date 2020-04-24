03f9e6c32dcc75ca7ad057abf30bbb9430daf89f
anthony <anthony.gensane@gmail.com>
2020-04-24 11:54:47 +0200

    Formattage des donnée de l'api et affichage à l'ecran

--- src/components/ComponentMain/NeoDisplay.jsx
+++ src/components/ComponentMain/NeoDisplay.jsx
@@ -3,15 +3,55 @@ import React from 'react';
 class NeoDisplay extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      arrFilter: []
+    };
+    this.formatNeosData = this.formatNeosData.bind(this);
+  }
+
+  formatNeosData(data) {
+    const formattedData = Object.keys(data);
+    console.log(data);
+    const neosMatrix = formattedData.map(date => {
+      // date ==> "2015-09-08"
+      return data[date].map(neo => {
+        // neo ==> {...}
+        return {
+          name: neo.name,
+          magnitude: neo.absolute_magnitude_h,
+          closeDate: neo.close_approach_data[0].close_approach_date,
+          danger: neo.is_potentially_hazardous_asteroid
+        };
+      });
+    });
+    const flattenMatrix = neosMatrix.reduce((carry, current) => {
+      // current ==> [{...},{...}]
+      current.forEach(neo => carry.push(neo));
+      return carry;
+    }, []);
+    console.log(flattenMatrix);
+    const filter = flattenMatrix.filter(item => item.danger === true);
+    console.log(filter);
+    this.setState({ arrFilter: filter });
   }
 
   render() {
+    const { arrFilter } = this.state;
     return (
       <div>
-        <button type="submit" onClick={() => console.log(this.props.data)}>
+        <button
+          type="submit"
+          onClick={() => {
+            this.formatNeosData(this.props.data);
+          }}
+        >
           click here
         </button>
+        <div>
+          {arrFilter.map((neo, i) => (
+            <button type="submit">i</button>
+          ))}
+        </div>
       </div>
     );
   }
