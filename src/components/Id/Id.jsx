b33edbfa944675a9407cd17c18676274ab6d1494
Justine810 <justine.gautreau@gmail.com>
2020-05-12 11:23:35 +0200

    ajout composant Id

---
+++ src/components/Id/Id.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../ComponentMain/GlobalContainer.css';
+
+function Id({ dataNeo, setDisplay }) {
+  return (
+    <div className="border position padding layout1">
+      <table className="color table">
+        <thead>
+          <tr>
+            <td>
+              <h2 className="color">Informations</h2>
+            </td>
+            <td>
+              <button type="submit" className="color fake-button" onClick={setDisplay}>
+                close [X]
+              </button>
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Nom :</td>
+            <td>{dataNeo.name}</td>
+          </tr>
+          <tr>
+            <td>Diamètre :</td>
+            <td>{`${dataNeo.size} Km`}</td>
+            <td>soit ....</td>
+          </tr>
+          <tr>
+            <td>Vitesse :</td>
+            <td>{`${dataNeo.speed} Km/h`}</td>
+          </tr>
+          <tr>
+            <td>Date d&apos; approche :</td>
+            <td>{dataNeo.closeDate}</td>
+          </tr>
+          <tr>
+            <td>Distance :</td>
+            <td>{`${dataNeo.distanceKm} Km`}</td>
+            <td>{`${dataNeo.distanceLunar} Terre => Lune`}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+Id.propTypes = {
+  dataNeo: PropTypes.shape.isRequired,
+  setDisplay: PropTypes.shape.isRequired
+};
+
+export default Id;
